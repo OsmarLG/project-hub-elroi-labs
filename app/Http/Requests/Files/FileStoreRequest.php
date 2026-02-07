e47747cf01164b25493aabfdb7d001c2ec71b153
Osmar Alejandro Liera Gomez <osmar2001lg@gmail.com>
2026-02-07 01:16:30 -0700

    cambios

--- app/Http/Requests/Files/FileStoreRequest.php
+++ app/Http/Requests/Files/FileStoreRequest.php
@@ -14,7 +14,7 @@ class FileStoreRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'file' => ['required', 'file', 'max:51200'], // 50MB
+            'file' => ['required', 'file', 'max:409600'], // 400MB
             'folder_id' => ['nullable', 'integer', 'exists:file_folders,id'],
             'title' => ['nullable', 'string', 'max:255'],
         ];
